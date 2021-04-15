8e16ecedcdd0bce26b0b659addfbcc6b501433a0
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-15 20:33:51 +0200

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -3,6 +3,8 @@ import requests as r
 from datetime import datetime
 import os
 
+# Checks it the file exists in the directory.
+
 if os.path.isfile('headlines.txt'):
     pass
 else:
@@ -11,12 +13,17 @@ else:
 
 now = datetime.now()
 
+# Gets the HTML of a webpage in text format.
 
 source = r.get('https://www.tijd.be/').text
 
 soup = BeautifulSoup(source, features='html.parser')
 
-match = soup.find_all('div', class_='c-articleteaser__title')
+#Finds all html code in a <div> with the class 'c-articleteaser__title'
+
+match = soup.find_all('div', class_='c-articleteaser__title') 
+
+# Writes all found info and the current time in the .txt file.
 
 with open('headlines.txt', 'a', encoding="utf-8") as docu:
     docu.write(f"De Tijd headlines van {now}\n\n")
