01616bc7ca959238112711b6e3ad3eef79e668fd
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-15 20:19:45 +0200

    commit rest

--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,19 @@
 #Websraping tutorial
+
+from bs4 import *
+import requests as r
+
+source = r.get('https://docs.python-requests.org/en/master/').text
+
+soup = BeautifulSoup(source, features="html.parser")
+
+
+
+# match var.find('div', class_='footer'))
+# match a div with an id of footer
+# check = article.p.text
+
+match = soup.find('div', class_='footer')
+
+finish = match.a.text
+print(finish)
