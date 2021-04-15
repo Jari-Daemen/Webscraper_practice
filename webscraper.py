25a51aa43e00cd98a7e7c0c6537d72f45036f768
Jari-Daemen <82100834+Jari-Daemen@users.noreply.github.com>
2021-04-15 19:22:48 +0200

    switch to test branch

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,14 @@
-# Webscraping tutorial
+# webscraping tutorial
 
 import bs4 as bs
-import requests as re
+import requests as r
+
+#source= r.get('url).text
 
 # format to see nesting
 # print(var.prettify())
+
+
+# match var.find('div', class_='footer'))
+# match a div with an id of footer
+# check = article.p.text
